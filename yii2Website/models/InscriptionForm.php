88e447035ec5e45b8fd526b461ab09e655d96412
BouEmi2333222 <143424324+BouEmi2333222@users.noreply.github.com>
2024-11-22 10:08:37 -0500

    Website

---
+++ yii2Website/models/InscriptionForm.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+
+class ConnexionForm extends Model
+{
+    public $username;
+    public $courriel;
+    public $password;
+
+    public function rules(){
+        
+    }
+}
